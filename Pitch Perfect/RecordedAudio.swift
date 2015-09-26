91ca3a447cd2486d3a1a4a25f868631ff80a2cf8
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-09-26 08:24:11 -0400

    code segue working. data model for audio

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Student on 9/26/15.
+//  Copyright © 2015 Charles Findlay. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    
+    var filePathUrl: NSURL!
+    var title: String!
+}
