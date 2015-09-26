611bde19ffa6db8e6844ba987c3a0f9c8c5a84f2
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-09-26 09:03:31 -0400

    Chipmunk effect workign

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -13,6 +13,9 @@ class PlaySoundsViewController: UIViewController {
 
     var audioPlayer: AVAudioPlayer!
     var receivedAudio:RecordedAudio!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +29,8 @@ class PlaySoundsViewController: UIViewController {
         
         audioPlayer = try? AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
         audioPlayer.enableRate = true
-        
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
         
     }
 
@@ -46,9 +50,31 @@ class PlaySoundsViewController: UIViewController {
         stopAudioPlayback()
         audioPlayer.rate = 1.5
         audioPlayer.play()
-
     }
     
+    
+    @IBAction func playChipmunkAudio(sender: AnyObject) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float) {
+        stopAudioPlayback()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        
+        audioPlayerNode.play()
+    }
     
     @IBAction func stopAudio(sender: AnyObject) {
         stopAudioPlayback()
@@ -58,6 +84,8 @@ class PlaySoundsViewController: UIViewController {
     func stopAudioPlayback() {
         audioPlayer.stop()
         audioPlayer.currentTime = 0
+        audioEngine.stop()
+        audioEngine.reset()
     }
 
     
